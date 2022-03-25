2fe3e4f75f570ba0b5e6990a1a381ebb760af71d
Fion <xmejiedo@gmail.com>
2022-03-25 11:06:59 +0800

    CHANGE: add in pages directory to hold pages and some basic pages and set the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Login, Homepage } from './pages'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<div>Login</div>} />
-        <Route path="/homepage" element={<div>Homepage</div>}></Route>
+        <Route index element={<Login />} />
+        <Route path="/homepage" element={<Homepage />}></Route>
         <Route path="*" element={<div>Error</div>} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+import Login from "./Login";
+import Homepage from "./Homepage";
+
+export { Login, Homepage }
